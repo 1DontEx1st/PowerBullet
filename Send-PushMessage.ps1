5f15e13d3b9e72aae252203dfe6259470b4f157b
1DontEx1st <github@idontexist.com>
2020-03-22 20:10:37 -0400

    Update Send-PushMessage.ps1

--- Send-PushMessage.ps1
+++ Send-PushMessage.ps1
@@ -3,43 +3,43 @@
    Use this Function to use the pushbullet service to send push messages to your browser, computer or device 
 .DESCRIPTION
    *****To use this function, you must register for a PushBullet account and copy your API key from under 'Account Settings'
-    
+   
    *****As of this release (Halloween Day, 2014) Lists and File Upload support are not yet functional
-    
+   
    Use this Function as a stand-alone push service, or embed within your other functions to perform dynamic messaging as needed
- 
-    
+
+   
 .EXAMPLE
    Send-PushMessage -msg "Hello world" -title "Mandatory first message text" 
- 
+
    #You will receive a push message on all of your configured devices with a "Hello World" message
-    
+   
 .EXAMPLE
    get-winevent -LogName System | Select -first 1500 | ? Id -eq '1074' | % {Send-PushMessage -Title "Reboot detected on System" -msg ("Reboot detected on " + $_.MachineName + "@ " +  $_.TimeCreated) -Type Message}
- 
- 
+
+
    #In this more complex scenario, if an event of 1074 (System Reboot) is noted within the most recent 1500 events, send a message using PushBullet with information from the event.  
 .NOTES
     If this function halts with a Warning message of "Please place your API Key within line 48 and try again", be certain to provide your API key on line 48.
 #>
 Function Send-PushMessage {
 [CmdletBinding(DefaultParameterSetName='Message')]
- 
- 
+
+
 param(
         [Parameter(Mandatory=$false,ParameterSetName="File")]$FileName,
         [Parameter(Mandatory=$true, ParameterSetName="File")]$FileType,
-        [Parameter(Mandatory=$true, ParameterSetName="File")]
+        [Parameter(Mandatory=$false, ParameterSetName="File")]
         [Parameter(Mandatory=$true, ParameterSetName="Link")]$url,
-         
+        
         [Parameter(Mandatory=$false,ParameterSetName="Address")]$PlaceName,
         [Parameter(Mandatory=$true, ParameterSetName="Address")]$PlaceAddress,
- 
+
         [Parameter(Mandatory=$false)]
-        [ValidateSet("Address","Message", "File", "List","Link")]
+        [ValidateSet("Address","Message","File","List","Link")]
         [Alias("Content")] 
         $Type,
-         
+        
         [switch]$UploadFile,
         [string[]]$items,
         $title="PushBullet Message",
@@ -50,17 +50,17 @@ begin{
     $devices = "https://api.pushbullet.com/v2/devices"
     $uploadRequestURL   = "https://api.pushbullet.com/v2/upload-request"
     $uploads = "https://s3.amazonaws.com/pushbullet-uploads"
- 
+
     $cred = New-Object System.Management.Automation.PSCredential ($api,(ConvertTo-SecureString $api -AsPlainText -Force))
- 
+
     if (($PlaceName) -or ($PlaceAddress)){$type = "address"}
- 
+
     if ($api -eq "***YourAPIHere***"){Write-warning "Please place your API Key within line 48 and try again"
         BREAK}
     }
- 
+
 process{
- 
+
     switch($Type){
     'Address'{Write-Verbose "Sending an Address"<#SendaMessagestuff#>
                 $body = @{
@@ -81,9 +81,9 @@ process{
                     type = "list"
                     title = $title
                     items = $items
-                     
+                    
                     } 
-                     
+                    
                     "body preview"
                     $body
                     #$body = $body | ConvertTo-Json 
@@ -95,42 +95,62 @@ process{
                     body = $msg
                     url = $url
                     } 
- 
+
              }
     'File'   {Write-Verbose "Sending a file "<#SendaFilestuff#>
-                 
               If ($UploadFile) {  
+                $fileLeaf = $filename | split-path -leaf
                 $UploadRequest = @{
-                    file_name = $FileName
-                    fileType  = $FileType
-                    }
-         
+                    file_name = $FileLeaf
+                    file_type  = $FileType
+                }
+        
                 #Ref: Pushing files https://docs.pushbullet.com/v2/pushes/#pushing-files
                 # "Once the file has been uploaded, set the file_name, file_url, and file_type returned in the response to the upload request as the parameters for a new push with type=file."
-             
+            
                 #Create Upload request first
                 $attempt = Invoke-WebRequest -Uri $uploadRequestURL -Credential $cred -Method Post -Body $UploadRequest -ErrorAction SilentlyContinue
                 If ($attempt.StatusCode -eq "200"){Write-Verbose "Upload Request OK"}
                 else {Write-Warning "error encountered, check `$Uploadattempt for more info"
                         $global:Uploadattempt = $attempt}
- 
-                #Have to include the data field from the full response in order to begin an upload
-                $UploadApproval = $attempt.Content | ConvertFrom-Json | select -ExpandProperty data 
-         
-                #Have to append the file data to the Upload request        
-                $UploadApproval | Add-Member -Name "file" -MemberType NoteProperty -Value ([System.IO.File]::ReadAllBytes((get-item C:\TEMP\upload.txt).FullName))
- 
-                #Upload the file and get back the url
-                #$UploadAttempt = 
-                #Invoke-WebRequest -Uri $uploads -Credential $cred -Method Post -Body $UploadApproval -ErrorAction SilentlyContinue
-                #Doesn't work...maybe invoke-restMethod is the way to go?
-             
-                Invoke-WebRequest -Uri $uploads -Method Post -Body $UploadApproval -ErrorAction SilentlyContinue
+
+#                #Have to include the data field from the full response in order to begin an upload
+#                $UploadApproval = $attempt.Content | ConvertFrom-Json | select -ExpandProperty data 
+                #Data is not needed, it's the other stuff
+                $json = $attempt.Content | ConvertFrom-Json
+                $uploadURL = $json.upload_url
+
+                $fileBytes = [System.IO.File]::ReadAllBytes($fileName);
+                $fileEnc = [System.Text.Encoding]::GetEncoding('iso-8859-1').GetString($fileBytes);
+                $boundary = [System.Guid]::NewGuid().ToString(); 
+                $LF = "`r`n";
+
+                $bodyLines = ( 
+                    "--$boundary",
+                    "Content-Disposition: form-data; name=`"file`"; filename=`"$fileLeaf`"",
+                    "Content-Type: application/octet-stream$LF",
+                    $fileEnc,
+                    "--$boundary--$LF" 
+                ) -join $LF
+
+                $uploaded = Invoke-WebRequest -uri $uploadURL -Method Post -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines #-Credential $cred
+
+                If ($uploaded.StatusCode -eq "204"){Write-Verbose "File upload OK"}
+                else {Write-Warning "error encountered, check `$Uploadattempt for more info"
+                        $global:Uploadattempt = $uploaded}
+            
                 #End Of Upload File scriptblock
+                $body = @{
+                    type = "file"
+                    file_name = $json.file_name
+                    file_type = $json.file_type
+                    file_url = $json.file_url
+                    body = $msg
+                    } 
                 }
             Else {
                 #If we don't need to upload the file
-                 
+                
                 $body = @{
                     type = "file"
                     file_name = $fileName
@@ -138,26 +158,25 @@ process{
                     file_url = $url
                     body = $msg
                     } 
-                 
+                
             }
             $global:UploadApproval = $UploadApproval
             BREAK
             #End of File switch
- 
+
             }
     #End of Switch Statement
     }
- 
+
     write-debug "Test-value of `$body before it gets passed to Invoke-WebRequest"
- 
+
     $Sendattempt = Invoke-WebRequest -Uri $PushURL -Credential $cred -Method Post -Body $body -ErrorAction SilentlyContinue
- 
+
     If ($Sendattempt.StatusCode -eq "200"){Write-Verbose "OK"}
         else {Write-Warning "error encountered, check `$attempt for more info"
               $global:Sendattempt = $Sendattempt  }
     }
- 
+
 end{$global:Sendattempt = $Sendattempt}
- 
- 
+
 }
